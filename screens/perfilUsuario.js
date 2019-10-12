7d05c3197df131284a685a3e65219ef10fc5aac2
Manuel <mac.jmanuel@gmail.com>
2019-10-12 02:29:52 -0500

    *added* Perfil usuario
    --corregir scroll / agregar iconos

--- screens/perfilUsuario.js
+++ screens/perfilUsuario.js
@@ -1,22 +1,44 @@
 import React, {Component} from 'react';
 import { View , Text, Image, TouchableOpacity, ScrollView} from 'react-native';
 import CircleButton from '../components/circleButton'
+import Card from '../components/cardDog'
+
 //Estilos
 import styles from '../styles/globalStyles'
 
 export default class userProfile extends Component {
     render() {
       return (
-        <View contentContainerStyle={styles.container}> 
-          <Image source = {require('../images/usuario.png')} style = {styles.profilePhoto}></Image>
-          <View style = {styles.contBotonCirc}>
-            <CircleButton/>
-            <CircleButton/>
-            <CircleButton/>
+        <View style = {styles.container}> 
+          <View style = {styles.contSup}>
+            <Image source = {require('../images/usuario.png')} style = {styles.profilePhoto}/>
+            <Text style = {styles.username}>jManuel</Text>
+            <View style = {styles.contBotonCirc}>
+              <View style = {styles.contBtnTxt}>
+                <CircleButton/>
+                <Text style = {styles.btnName}>Agregar</Text>
+              </View>
+              <View style = {styles.contBtnTxt}>
+                <CircleButton/>
+                <Text style = {styles.btnName}>Editar</Text>
+              </View>
+              <View style = {styles.contBtnTxt}>
+                <CircleButton/>
+                <Text style = {styles.btnName}>Ajustes</Text>
+              </View>
+            </View>
           </View>
-          <ScrollView contentContainerStyle={styles.container}>
-            
-          </ScrollView>
+          <View style = {styles.contInf}>
+            <ScrollView contentContainerStyle={styles.dogContainer}>
+              <Text style = {styles.subtitle}>Mis perros</Text>
+              <Card imageUri = {require('../images/dog.jpg')}/>
+              <Card imageUri = {require('../images/dog.jpg')}/>
+              <Card imageUri = {require('../images/dog.jpg')}/>
+              <Card imageUri = {require('../images/dog.jpg')}/>
+              <Card imageUri = {require('../images/dog.jpg')}/>
+              <Card imageUri = {require('../images/dog.jpg')}/>
+            </ScrollView>
+          </View>
         </View>
       );
     }
